aab1664dcb7a5522823dfbf2ae83f4b49c29cc62
opendroid-Team <opendroid2014@gmail.com>
2014-02-21 20:30:40 +0100

    test rc_models

--- lib/python/Components/RcModel.py
+++ lib/python/Components/RcModel.py
@@ -8,10 +8,11 @@ class RcModel:
 	RCTYPE_VU = 4
 	RCTYPE_ODINM9 = 5
 	RCTYPE_ODINM7 = 6
-        RCTYPE_E3HD = 7
-        RCTYPE_IQON = 8
-        RCTYPE_XP1000 = 9
-        RCTYPE_INI2 = 10
+	RCTYPE_E3HD = 7
+	RCTYPE_ET4X00 = 8
+	RCTYPE_INI2 = 9
+	RCTYPE_IQON = 10
+	RCTYPE_XP1000 = 11
 
 	def __init__(self):
 		self.currentRcType = self.RCTYPE_DMM
@@ -43,20 +44,22 @@ class RcModel:
 					self.currentRcType = self.RCTYPE_ET6X00
 				elif rc == '8':
 					self.currentRcType = self.RCTYPE_VU
-                                elif rc == '10':
-					self.currentRcType = self.RCTYPE_E3HD
-                                elif rc == '11':
-					self.currentRcType = self.RCTYPE_IQON
-                                elif rc == '11':
-					self.currentRcType = self.RCTYPE_XP1000
-                                elif rc == '11':
-					self.currentRcType = self.RCTYPE_INI2
 				elif rc == '9':
 					self.currentRcType = self.RCTYPE_ET9500
+					elif rc == '10':
+					self.currentRcType = self.RCTYPE_ET4X00
 				elif model == 'odinm9':
 					self.currentRcType = self.RCTYPE_ODINM9
 				elif model == 'odinm7':
 					self.currentRcType = self.RCTYPE_ODINM7
+					elif model == 'e3hd':
+					self.currentRcType = self.RCTYPE_E3HD
+					elif model == 'ini2':
+					self.currentRcType = self.RCTYPE_INI2
+					elif model == 'iqon':
+					self.currentRcType = self.RCTYPE_IQON
+					elif model == 'xp1000':
+					self.currentRcType = self.RCTYPE_XP1000
 				elif os.path.exists('/proc/stb/info/vumodel'):
 					self.currentRcType = self.RCTYPE_VU
 
@@ -65,18 +68,12 @@ class RcModel:
 			return '/usr/share/enigma2/rc_models/et9x00/'
 		elif self.currentRcType == self.RCTYPE_ET9500:
 			return '/usr/share/enigma2/rc_models/et9500/'
+			elif self.currentRcType == self.RCTYPE_ET4x00:
+			return '/usr/share/enigma2/rc_models/et4x00/'
 		elif self.currentRcType == self.RCTYPE_ET6X00:
 			return '/usr/share/enigma2/rc_models/et6x00/'
 		elif self.currentRcType == self.RCTYPE_VU:
 			return '/usr/share/enigma2/rc_models/vu/'
-                elif self.currentRcType == self.RCTYPE_E3HD:
-			return '/usr/share/enigma2/rc_models/e3hd/'
-                elif self.currentRcType == self.RCTYPE_IQON:
-			return '/usr/share/enigma2/rc_models/iqon/'
-                elif self.currentRcType == self.RCTYPE_XP1000:
-			return '/usr/share/enigma2/rc_models/xp1000/'
-                elif self.currentRcType == self.RCTYPE_INI2:
-			return '/usr/share/enigma2/rc_models/ini2/'
 		elif self.currentRcType == self.RCTYPE_ODINM9:
 			return '/usr/share/enigma2/rc_models/odinm9/'
 		elif self.currentRcType == self.RCTYPE_ODINM7:
