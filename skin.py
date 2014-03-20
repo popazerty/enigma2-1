d7e875aa395a6db14e29a8f3a2a2aa0b9d4e43ee
opendroid-Team <opendroid2014@gmail.com>
2014-03-20 03:09:35 +0100

    edit skin.py

--- skin.py
+++ skin.py
@@ -72,7 +72,7 @@ DEFAULT_SKIN = "mega32/skin.xml"
 # on SD hardware, PLi-HD will not be available
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
 	# in that case, fallback to PLi-HD (which is an SD skin)
-	DEFAULT_SKIN = "PLi-HD/skin.xml"
+	DEFAULT_SKIN = "skin.xml"
 config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)
 
 profile("LoadSkin")
